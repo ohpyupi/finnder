e252f1299b2024e4d96ff97153796a5a95aa9fd9
Hoseong Lee <ohpyupi@gmail.com>
2018-02-10 14:28:07 -0800

    Hid the confidential credentials.

--- config/variables.js
+++ config/variables.js
@@ -2,13 +2,9 @@ require('dotenv').load();
 
 const _sharable = {
 	PORT: process.env.PORT || 3000,
-  TWILIO: {
-    ACCOUNT_SID: 'AC015e98a7ea4bf6fad5406e6b58cfee97',
-    AUTH_TOKEN: '1baaa6b39a24ba103d4bb219d9470d9a'
-  },
-  MONGO_DB_USER: 'root',
-  MONGO_DB_PASSWORD: '3yugrClaWa65KNu6Bx89',
-  MONGO_DB_URI: `mongodb://root:3yugrClaWa65KNu6Bx89@ds231658.mlab.com:31658/heroku_5m03dtnw`,
+  TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
+  MONGO_DB_URI: `mongodb://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@ds231658.mlab.com:31658/heroku_5m03dtnw`,
 };
 
 const dev = Object.assign({}, _sharable);
