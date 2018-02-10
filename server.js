dd69f52208c4974ea7563952285874411a86f9f2
Patrick San Juan <psj.webdev@gmail.com>
2018-02-10 14:51:43 -0800

    scaffold twilio response functionality

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const VAR = require('./config/variables');
+const twilio = require('twilio');
+const MessagingResponse = twilio.twiml.MessagingResponse;
+const client = twilio(VAR.TWILIO.ACCOUNT_SID, VAR.TWILIO.AUTH_TOKEN);
 
 mongoose.connect(VAR.MONGO_DB_URI);
 const db = mongoose.connection;
@@ -20,6 +23,19 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res, next)=>{
 	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  client.messages(req.body.SmsMessageSid)
+    .fetch()
+    .then((result) => console.log(result))
+
+  twiml.message('The Robots are coming! Head for the hills!');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
 });
 
 app.use((err, req, res, next)=>{
