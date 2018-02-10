ce5efe4b9ca42eceac87fcdf6ea0f9d08297570c
Patrick San Juan <psj.webdev@gmail.com>
2018-02-10 13:20:47 -0800

    config twilio

--- config/variables.js
+++ config/variables.js
@@ -2,6 +2,10 @@ require('dotenv').load();
 
 const _sharable = {
 	PORT: process.env.PORT || 3000,
+  TWILIO: {
+    ACCOUNT_SID: 'AC015e98a7ea4bf6fad5406e6b58cfee97',
+    AUTH_TOKEN: '1baaa6b39a24ba103d4bb219d9470d9a'
+  }
 };
 
 const dev = Object.assign({}, _sharable);
