1255ee5e3640a7a41f5ce4057e5022746312f9d3
Patrick San Juan <psj.webdev@gmail.com>
2018-02-11 12:04:32 -0800

    added price feature

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const client = twilio(VAR.TWILIO_ACCOUNT_SID, VAR.TWILIO_AUTH_TOKEN);
 const deployCron = require('./crons');
 const buyFish = require('./src/buyFish');
 const buyList = require('./src/buyList');
+const price = require('./src/price');
 
 const db = require('./config/db.js');
 const shcedules = deployCron();
@@ -33,15 +34,23 @@ app.post('/sms', (req, res) => {
       const message = result.body.toLowerCase();
       const buyRegx = /\bbuy\b/;
       const buyListRegx = /\bbuylist\b/;
+      const priceRegx = /\bprice\b/;
       console.log(message);
 
       if(buyRegx.test(message)) {
         console.log('in buyFish');
         buyFish(req.body, message, res);
-      } else if(buyRegx.test(message)) {
+      } else if(buyListRegx.test(message)) {
+        console.log('in buyList');
         buyList(message, res);
       } else if(priceRegx.test(message)) {
-        price(message, res)
+        console.log('in price');
+        price(message, res);
+      } else {
+        // Default response... possibly add menu of features here.
+        twiml.message('Sorry i don\'t understand...');
+        res.writeHead(200, {'Content-Type': 'text/xml'});
+        res.end(twiml.toString());
       }
     });
 });
